5cc09e784de1ee44fd069908e5459e64b5a845cd
Evgenii Eremin <eremineu@gmail.com>
2025-01-27 10:39:20 +0100

    JS/TS gRPC files generation fixed

---
+++ js/ots/service_pb.d.ts
@@ -0,0 +1,13 @@
+// package: ots
+// file: ots/service.proto
+
+import * as jspb from "google-protobuf";
+import * as ots_bettor_pb from "../ots/bettor_pb";
+import * as ots_ticket_pb from "../ots/ticket_pb";
+import * as ots_ticket_cancel_pb from "../ots/ticket_cancel_pb";
+import * as ots_player_risk_score_pb from "../ots/player_risk_score_pb";
+import * as ots_resolve_foreign_events_in_ticket_pb from "../ots/resolve_foreign_events_in_ticket_pb";
+import * as ots_ticket_ack_pb from "../ots/ticket_ack_pb";
+import * as ots_ticket_max_stake_pb from "../ots/ticket_max_stake_pb";
+import * as ots_ticket_result_pb from "../ots/ticket_result_pb";
+
--- scripts/js.sh
+++ scripts/js.sh
@@ -5,22 +5,29 @@ echo "GENERATING JAVASCRIPT"
 JS_PATH=./js
 DEF=./proto
 
+# Get the absolute path to the project directory
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
 # Install dependencies
 npm i
+
+# Create JS output directory if it doesn't exist
+mkdir -p ${JS_PATH}
 
 # Clean folder for generated data
 rm -rf ${JS_PATH}/*
 
 # Path to this plugin
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
+PROTOC_GEN_JS_PATH="./node_modules/.bin/protoc-gen-js"
 
 # Generate protobuf
-protoc -I ./${DEF} \
-    --plugin=protoc-gen-grpc=node_modules/grpc-tools/bin/grpc_node_plugin \
+protoc \
+    --plugin="protoc-gen-js=${PROTOC_GEN_JS_PATH}" \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${JS_PATH}" \
-    --grpc_out=${JS_PATH} \
     --ts_out="${JS_PATH}" \
+    -I ./${DEF} \
     ./${DEF}/*/*.proto ./${DEF}/*/*/*.proto
 
 echo "JAVASCRIPT DONE"
